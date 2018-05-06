ee0fe7384bf2034a0289cb30044bea08e1799f22
Kris Gesling <krisgesling@gmail.com>
2018-05-06 09:36:09 +0930

    Add unique key to button components; Cleanup LookupWord

--- src/components/LookupWord.js
+++ src/components/LookupWord.js
@@ -6,12 +6,11 @@ function lookupWord(inputWord) {
 
   const sanitizedInputWord = inputWord.toLowerCase();
   const suggestions = Object.keys(dictionary).filter((word) => {
-    return word.substr(0, sanitizedInputWord.length).toLowerCase() == sanitizedInputWord
+    return word.substr(0, sanitizedInputWord.length).toLowerCase() === sanitizedInputWord
   })
   const definition = (dictionary[sanitizedInputWord])
     ? dictionary[sanitizedInputWord].En
     : ''
-  // const suggestions = ['baḏikan', 'baḏipaḏi'];
  return {'definition': definition, 'suggestions': suggestions};
 }
 
--- src/components/WordSuggestions.js
+++ src/components/WordSuggestions.js
@@ -6,12 +6,12 @@ class WordSuggestions extends Component {
     const list = (words.length > numOfBtns)
       ? words.slice(0,numOfBtns)
       : words
-    return list.map((word) => {
-      return this.createButton(word)
+    return list.map((word, i) => {
+      return this.createButton(word, i+1)
     })
   }
-  createButton(word) {
-    return <button onClick={() => this.selectSuggestion(word)}>{word}</button>
+  createButton(word, i) {
+    return <button key={i} onClick={() => this.selectSuggestion(word)}>{word}</button>
   }
   selectSuggestion(word) {
     this.props.onSelectSuggestion({selectedSuggestion: word})
