e71ab8723bb545fd9d0b34d6f5f4d519cd09b9eb
Kris Gesling <krisgesling@gmail.com>
2018-05-07 21:47:46 +0930

    Add yolngu typing via ; modifier

--- README.md
+++ README.md
@@ -1,22 +1,33 @@
 # Yolŋu Matha Language Tools
-This is a procrastination project, because writing an app to translate things is more fun than doing a translation assignment.
+Demo version live at: https://yolngu-matha-tools.herokuapp.com/
 
-Returns real-time definitions for single words.
-Suggests complete words to choose from based on existing input.
+[Hit me up on twitter](https://www.twitter.com/krisgesling) if you find it helpful.
+
+Returns real-time definitions for single words and suggests complete words to choose from.
+
+Type in yolŋu characters by first typing ';' then the corresponding key.
+'a' = 'ä'
+'d' = 'ḏ'
+'j' = 'ŋ'
+'l' = 'ḻ'
+'n' = 'ṉ'
+'t' = 'ṯ'
+'A' = 'Ä'
+'D' = 'Ḏ'
+'J' = 'Ŋ'
+'L' = 'Ḻ'
+'N' = 'Ṉ'
+'T' = 'Ṯ'
 
 Full dictionary is not publicly available.
 
-
-You can see the progress at:
-https://yolngu-matha-tools.herokuapp.com/
-
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+This is a procrastination project, because writing an app to translate things is more fun than doing a translation assignment.
 
 ## To do
 - [x] Fix carriage return error in scraper
-- [x] Sanitize scraper input - replace " for '
-- [ ] Implement localStorage for dictionary file - hash/versioning to see if it needs updating?
+- [x] Sanitize scraper input
 - [x] Add text autocomplete suggestions
+- [ ] Add yolngu keyboard functionality to text input
+- [ ] Implement localStorage for dictionary file - hash/versioning to see if it needs updating?
 - [ ] Add multi-word input to ordered glossary
 - [ ] Add documentation for users
-- [ ] Add yolngu keyboard functionality to text input
--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -6,13 +6,57 @@ class UserInput extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { 'userInput': '', 'suggestions': [''] };
+    this.state = { 'userInput': '', 'suggestions': [''], 'modifierKey': false };
     this.inputUpdate = this.inputUpdate.bind(this);
+    this.selectedSuggestion = this.selectedSuggestion.bind(this);
+    this.yolnguKeyboard = this.yolnguKeyboard.bind(this);
+  }
+
+  yolnguKeyboard(key) {
+    let newKey, modified;
+    if (this.state.modifierKey) {
+      const keyMap = {
+        'a': 'ä',
+        'd': 'ḏ',
+        'j': 'ŋ',
+        'l': 'ḻ',
+        'n': 'ṉ',
+        't': 'ṯ',
+        'A': 'Ä',
+        'D': 'Ḏ',
+        'J': 'Ŋ',
+        'L': 'Ḻ',
+        'N': 'Ṉ',
+        'T': 'Ṯ'
+      }
+      if (keyMap[key]) {
+        newKey = keyMap[key]
+        modified = true;
+      } else {
+        modified = false
+      }
+      this.setState({'modifierKey': false})
+    } else {
+      modified = false;
+      if (key === ';') this.setState({ 'modifierKey': true})
+    }
+    return {
+      'key': (newKey ? newKey : key),
+      'modified': modified
+    };
   }
 
   inputUpdate(e) {
-    const newValue = e.target.value;
-    const inputLookup = lookupWord(newValue)
+    // TODO find diff between newValue and state.userInput
+    const inpVal = e.target.value;
+    const charIndex = inpVal.length-1
+    let newInputArr = inpVal.split('');
+    const yolnguValue = this.yolnguKeyboard(inpVal[charIndex]);
+    yolnguValue.modified
+      ? newInputArr.splice(charIndex-1, 2, yolnguValue.key)
+      : newInputArr[charIndex] = this.yolnguKeyboard(inpVal[charIndex]).key;
+    const newValue = newInputArr.join('')
+    const inputLookup = lookupWord(newValue);
     this.setState({
       'userInput': newValue,
       'definition': inputLookup.definition,
@@ -21,15 +65,15 @@ class UserInput extends Component {
   }
 
   selectedSuggestion(word) {
-    const mockEvent = {'target': {'value': word.selectedSuggestion}}
-    this.inputUpdate(mockEvent)
+    const mockEvent = {'target': {'value': word.selectedSuggestion}};
+    this.inputUpdate(mockEvent);
   }
 
   render() {
     return (
       <div className="user-input">
         <textarea value={this.state.userInput} onChange={this.inputUpdate} />
-        <WordSuggestions words={this.state.suggestions} onSelectSuggestion={this.selectedSuggestion.bind(this)}/>
+        <WordSuggestions words={this.state.suggestions} onSelectSuggestion={this.selectedSuggestion}/>
         <p>{this.state.definition}</p>
       </div>
     );
