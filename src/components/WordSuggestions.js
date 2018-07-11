91ffd926aea7641c999990cece6404b6faf5bd14
Kris Gesling <krisgesling@gmail.com>
2018-07-12 07:59:04 +0930

    Refactor; Remove hide table on empty list

--- src/components/WordSuggestions.js
+++ src/components/WordSuggestions.js
@@ -1,51 +1,36 @@
 import React, { Component } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group'
 
 class WordSuggestions extends Component {
-  listSuggestions(words) {
-    const list = Object.keys(words)
-    // TODO why am I iterating over object.keys twice for no reason?
-    return list.map((word, i) => {
-      return this.createTableRow(word, i+1)
-    })
-  }
-  createTableRow(word, i) {
-    return (
-      <tr key={i} onClick={() => this.selectSuggestion(word)}>
-        <td>{word}</td>
-        <td>{this.props.words[word]}</td>
-      </tr>
-    )
-  }
   selectSuggestion(word) {
     this.props.onSelectSuggestion(word)
   }
   render() {
-    if (Object.keys(this.props.words).length > 0) {
+    const suggestions = Object.keys(this.props.words).map((word, i) => {
       return (
-        <div className="word-suggestions">
-          <h3>Did you mean...</h3>
-          <table>
-            <thead>
-              <tr>
-                <th>Yolŋu Matha</th>
-                <th>English</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.listSuggestions(this.props.words)}
-            </tbody>
-          </table>
-        </div>
-      );
-    } else {
-      return (
-        <div className="word-suggestions">
-        </div>
+        <tr key={i+1} onClick={() => this.selectSuggestion(word)}>
+          <td>{word}</td>
+          <td>{this.props.words[word]}</td>
+        </tr>
       )
-    }
-
+    });
+    return (
+      <div className="word-suggestions">
+        <h3>Did you mean...</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Yolŋu Matha</th>
+              <th>English</th>
+            </tr>
+          </thead>
+          <tbody>
+            {suggestions}
+          </tbody>
+        </table>
+      </div>
+    );
   }
-
 }
 
 export default WordSuggestions;
